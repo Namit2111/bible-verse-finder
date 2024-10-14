3a6fd94b100c652040ce278b7dc68d7c89599777
rohit rai <rohitrai170904@gmail.com>
2024-10-14 19:15:54 +0530

    find-similar-verse-button-changed

--- bible-app/app/api/similarity/route.ts
+++ bible-app/app/api/similarity/route.ts
@@ -18,7 +18,7 @@ export async function POST(request: Request) {
 
     const data = await response.json();
     return NextResponse.json(data);
-  } catch (error) {
+  } catch  {
     return NextResponse.json({ error: 'An error occurred while fetching data' }, { status: 500 });
   }
 }
