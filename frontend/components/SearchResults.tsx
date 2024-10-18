b4bf7b70de00944aff85ce189636a3dcd43ed223
fortune710 <fortune0208@yahoo.com>
2024-10-18 19:51:20 -0400

    fix: diabled copy button while translation is loading

--- frontend/components/SearchResults.tsx
+++ frontend/components/SearchResults.tsx
@@ -71,34 +71,35 @@ function SearchResultListItem ({
     return (
         <li className="p-4 bg-white rounded-xl shadow">
             <div className="flex justify-between items-start">
-            <div className="min-w-[270px]">
-                {
-                    isLoading ? <Skeleton className="h-4 w-[240px]" /> :
-                    <>
-                        <p className="text-md font-semibold mb-2">
-                            <span className="text-siteColor font-bold">Verse:</span> 1
-                            John {translatedVerse}
-                        </p>
-                        <p className="text-md font-semibold">
-                            <span className="text-siteColor font-bold">
-                                Similarity:
-                            </span>{" "}
-                            {decimalToPercentage(similarity)}
-                        </p>                    
-                    </>
-                }
-            </div>
-            <button 
-                onClick={copyText}
-                className="p-2 text-gray-500 hover:text-gray-700 transition-colors"
-                aria-label="Copy to clipboard"
-            >
-                {isCopied ? (
-                <Check className="w-5 h-5 text-green-500" />
-                ) : (
-                <Copy className="w-5 h-5" />
-                )}
-            </button>
+                <div className="min-w-[270px]">
+                    {
+                        isLoading ? <Skeleton className="h-4 w-[240px]" /> :
+                        <>
+                            <p className="text-md font-semibold mb-2">
+                                <span className="text-siteColor font-bold">Verse:</span> 1
+                                John {translatedVerse}
+                            </p>
+                            <p className="text-md font-semibold">
+                                <span className="text-siteColor font-bold">
+                                    Similarity:
+                                </span>{" "}
+                                {decimalToPercentage(similarity)}
+                            </p>                    
+                        </>
+                    }
+                </div>
+                <button 
+                    onClick={copyText}
+                    disabled={isLoading}
+                    className="p-2 text-gray-500 hover:text-gray-700 transition-colors"
+                    aria-label="Copy to clipboard"
+                >
+                    {isCopied ? (
+                    <Check className="w-5 h-5 text-green-500" />
+                    ) : (
+                    <Copy className="w-5 h-5" />
+                    )}
+                </button>
             </div>
         </li>
 
