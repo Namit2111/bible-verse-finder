57d03b615390d19ba8cbd802f5a970e2d8bf3410
justinhSE <justinh.tech1@gmail.com>
2024-11-08 17:55:31 -0500

    Closes #63

---
+++ frontend/components/firebase/firebase.tsx
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_apiKey,
+  authDomain: process.env.NEXT_PUBLIC_authDomain,
+  projectId: process.env.NEXT_PUBLIC_projectId,
+  storageBucket: process.env.NEXT_PUBLIC_storageBucket,
+  messagingSenderId: process.env.NEXT_PUBLIC_messagingSenderId,
+  appId: process.env.NEXT_PUBLIC_appId,
+  measurementId: process.env.NEXT_PUBLIC_measurementId,
+};
+
+//initialize firebase
+export const app = initializeApp(firebaseConfig);
+export const firestore = getFirestore(app);
