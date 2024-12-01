41032d9171b62065afaba8cb941d179f56f4ca71
Megha30501 <m_patel39@u.pacific.edu>
2024-11-30 21:40:07 -0800

    Added design for signup page

--- frontend/app/signup/page.tsx
+++ frontend/app/signup/page.tsx
@@ -1,12 +1,17 @@
-'use client';
 import React from 'react'
-import { signInWithGoogle } from '@/components/SignUP/SignInWithGoogle'
+import Header from "../../components/Header";
+import { SignInWithGoogle } from '@/components/SignUP/SignInWithGoogle'
 
 export default function Signup(){
     return(
-        <div>
-      <h1>Sign In with Google</h1>
-      <button onClick={signInWithGoogle}>Sign in with Google</button>
-    </div>
+      <section className='min-h-screen bg-black text-white'>
+        <Header/>
+        <div className="text-center p-6 mx-auto md:max-w-[448px]">
+        <h1 className="text-3xl font-bold mb-4">Create Your Account</h1>
+        <div className='mt-10 flex flex-col items-center gap-4 md:mt-14'>
+          <SignInWithGoogle/>
+          </div>
+        </div>
+      </section>
     )
 }
--- frontend/components/SignUP/SignInWithGoogle.tsx
+++ frontend/components/SignUP/SignInWithGoogle.tsx
@@ -1,12 +1,17 @@
+"use client";
+
 import { GoogleAuthProvider, getAuth, signInWithPopup } from 'firebase/auth';
 import { collection, doc, setDoc } from 'firebase/firestore';
 import { app, firestore } from '@/firebase/firebase';
+import GoogleIcon from '../icons/GoogleIcon';
 
 // Variables
 const auth = getAuth(app);
 
-export const signInWithGoogle = async (): Promise<void> => {
+export const SignInWithGoogle = () => {
     const googleProvider = new GoogleAuthProvider();
+    
+    async function signInWithGoogle() {
     try {
         const userCredential = await signInWithPopup(auth, googleProvider);
         const user = userCredential.user;
@@ -24,7 +29,25 @@ export const signInWithGoogle = async (): Promise<void> => {
             signInMethod: 'google', // Method of signing in
         });
         console.log('User information saved to Firestore');
+
     } catch (error) {
         console.error('Error during Google sign-in:', error);
     }
+    }
+    return (
+        <>
+        
+        <div className="relative items-center justify-center inline-flex group">
+			<div className="absolute transition-all duration-200 rounded-lg -inset-px bg-gradient-to-r from-cyan-500 to-purple-500 group-hover:shadow-lg group-hover:shadow-cyan-500/50"></div>
+					<button
+						onClick={signInWithGoogle}
+						className="relative inline-flex items-center justify-center capitalize h-12 w-64 gap-5 text-base font-bold text-white bg-black border border-transparent rounded-lg"
+					>
+                        <GoogleIcon/>
+						  Continue with Google
+					</button>
+			</div>
+		
+        </>
+    );
 };
